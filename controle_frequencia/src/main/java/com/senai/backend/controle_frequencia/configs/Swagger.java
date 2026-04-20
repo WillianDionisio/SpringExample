14d86538d4e91d7daa8771a16d96c6ed163221eb
Willian Gonçalves Pereira Dionísio <pet0163507@aluno.firjansenaisesi.com.br>
2026-04-20 16:47:14 -0300

    Swagger

---
+++ controle_frequencia/src/main/java/com/senai/backend/controle_frequencia/configs/Swagger.java
@@ -0,0 +1,15 @@
+package com.senai.backend.controle_frequencia.configs;
+
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@Configuration
+@OpenAPIDefinition(info = @Info(title = "Spring Example", version = "1.0", description = "API para o sistema Spring Example")
+
+)
+
+public class Swagger {
+
+}
